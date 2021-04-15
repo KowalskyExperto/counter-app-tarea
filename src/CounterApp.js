9d9795651bec29e4d815a156ca1d2bd2536e367c
KowalskyExperto <kowalskyexperto@gmail.com>
2021-04-15 18:14:00 -0500

    Cambio en la estructura de los archivos y código.

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import Contador from './components/Contador';
+import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {useCouter} from './hooks/useCounter'
+import { useForm } from './hooks/useForm';
 
-const CounterApp = ({value}) => {
+export const CounterApp = ({initialState=10,cambio=1}) => {
+    const {counter:state,increment,decrement,reset} = useCouter(initialState)
+    const [formValues, handleInputChange] = useForm({
+        numero: 1,
+    },['numero']);
 
+    const {numero} = formValues;
+
+    useEffect(() => {
+        console.log('Numero Cambio');
+    }, [numero])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    
     return (
         <>
-        <Contador value={value} cambio={5}/>
+        <div>
+            <h1>Contador</h1>
+            <h1>{state}</h1>
+            <button onClick={() => increment((numero) ? parseInt(numero):0)}>+{numero}</button>
+            <button onClick={() => reset()}> Reset </button>
+            <button onClick={() => decrement((numero) ? parseInt(numero):0)}>-{numero}</button>
+        </div>
+        <div>
+            <h1>Cambio</h1>
+            <h1>{(numero) ? numero:'Escriba algún numero'}</h1>
+            <form onSubmit={handleSubmit}>
+                <input type='number' name='numero' value={numero} onChange={handleInputChange}/>
+            </form>
+        </div>
         </>
-        
     );
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number,
 }
 
 export default CounterApp;
